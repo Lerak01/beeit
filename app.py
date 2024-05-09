6dd47e7634b8be9b1ade7da010036bc69d3227f5
Karel Martének <karel-m@seznam.cz>
2024-05-09 13:39:07 +0200

    Pridal jsem funkci na odecitani

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ def secti (a: float, b: float) -> float:
 
 def main():
   print(secti(a=5, b=3))
+
+# git add . # prepise vsechny soubory v githubu
+# git add app.py #prepise app.py v githubu  
+# git commit -am "pridal jsem funcki na odecitani" # vytvori a prepise commit a koment v githubu(prikaz na vsechno)
